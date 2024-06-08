e925d742cbbc4087c3b52db1729e5449effe4a2d
hengamehsh59 <hengamehsh59@gmail.com>
2024-06-08 19:01:14 +0330

    Add files via upload

---
+++ main (11).cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num;
+char ch;
+cout<<"please inter a number:";
+cin>>num;
+cout<<"please enter a character:";
+cin>>ch;
+for(int i=0;i<num;i++)
+cout<<ch<<endl;
+return 0;
+}
